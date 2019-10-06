965e0467fd1e403eb125c884e992b39931f65d48
KHA <gamerx6900@gmail.com>
2019-10-07 00:54:35 +0300

    Second activity enabled via cardboard 10/7/2019 monday 12:37am

--- app/src/main/java/com/example/uidashboard/MainActivity.java
+++ app/src/main/java/com/example/uidashboard/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.uidashboard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    CardView cardview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupCardview();
     }
+
+   private void setupCardview(){
+        cardview = findViewById(R.id.card1);
+        cardview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Activity2.class);
+                startActivity(intent);
+            }
+        });
+   }
 }
